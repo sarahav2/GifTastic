07bdbab06f57775e9b6144cefbb25fef55aa2596
Sarah Avwata <sarahavwata@Sarahs-MacBook-Pro.local>
2017-12-18 21:58:34 -0500

    push

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -1,95 +1,105 @@
-  var person = ["Khal Drogo", "Little Finger", "Arya Stark", "Tyrion Lannister", "Jon Snow", "Khalessi"]; //topics array 
+$(document).ready(function() {
 
-function makebutton (){
-    $("#buttondiv").empty();
-   
-    for (var i = 0; i <items.length; i++) {
-      var buttons= $("<button>")
-      buttons.attr ("data-person", items [i])
-      buttons.text (items [i])
-      buttons.addClass ("btn")
-      $("#buttondiv").append(buttons);
-   
-    }
-   }
+  // Array
+  
+	var person = ["Khal Drogo", "Little Finger", "Khalessi", "Jon Snow", "Tyrion Lannister", "Arya Stark"];
+		
+	function renderButtons() {
 
-makebutton();
+		// Deletes the buttons before adding new buttons
+		$('#personButtons').empty();
 
-$(document).on("click", "#add-item",function(){
- event.preventDefault();
-var newitem = $("#form-input").val();
-items.push(newitem);
-makebutton();
-});
+		// Loops through 
+		for (var i = 0; i < person.length; i++) {
+			var a = $('<button>');
+      a.text(topics[i].text); 					
+      // Provides the initial button text
+			a.addClass('rugby'); 						// Added a class
+      a.attr('data-search', person[i].search); 	
+      // Added a data-attribute for searching
+      $('#personButtons').append(a); 				
+      // Added the button to the HTML
+		}
 
-//Click for images
-$(document).on("click", ".gifimage", function (){
- //grab the state from the image
-var state = $(this).attr("data-state");
-console.log("this is the state: " + state);
-//We need to flip the state from still to animate
-if(state === "still"){
- $(this).attr("src", $(this).attr("data-animate"));
- $(this).attr("data-state", "animate");
-}
+		$("#newButton").focus();					
+	}
 
-else{
- $(this).attr("src", $(this).attr("data-still"));
- $(this).attr("data-state", "still");
-
-}
-
-});
-
-   // Adding click event for class
-   $(document).on("click", ".btn", function() {
-
-   
-      var person = $(this).attr("data-person");
+	renderButtons();
 
 
-     // Constructing a queryURL
-     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + person + "&api_key=dc6zaTOxFJmzC&limit=10";
+	$('#submitButton').on('click', function() {
+		var userButton = $('#newButton').val().trim();
+		
+		// Prevents an empty button from being made
+		if (userButton != ""){
+      var replaced = userButton.split(" ").join("+");	
+      	// Replaces whitespaces with "+" for searching
+			person[person.length] = {"text" : userButton, "search" : replaced};	
+			console.log(topics);
+			renderButtons();
+		}
 
-     // Performing an AJAX request with the queryURL
-     $.ajax({
-         url: queryURL,
-         method: "GET"
-       })
-       // After data comes back from the request
-       .done(function(response) {
-         console.log(queryURL);
+		$("#newButton").val("") 			
+			.focus();					
 
-         console.log(response);
+		// We have this line so that users can hit "enter" instead of clicking on the button and it won't move to the next page
+		return false;
+	});
 
-         var results = response.data;
-
-         // Looping through each result item
-         for (var i = 0; i < results.length; i++) {
-
-        
-           var personDiv = $("<div>");
-
-           var p = $("<p>").text("Rating: " + results[i].rating);
-
-           // Creating and storing an image tag
-           var personImage = $("<img>");
-           // Setting the src attribute of the image to a property pulled off the result item
-           personImage.attr("src", results[i].images.fixed_height_still.url);
-           personImage.attr("data-animate", results[i].images.fixed_height.url);
-           personImage.attr("data-still", results[i].images.fixed_height_still.url);
-           personImage.attr("data-state", "still");
-           personImage.addClass("gifimage");
+	
+	$(document).on('click', 'button',  function() {
+	    var b = $(this).data('search');		// 'this' refers to the button that was clicked
+	    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + person + "&api_key=dc6zaTOxFJmzC&limit=10";
+      console.log(queryURL);
           
+// Performing an AJAX request with the queryURL
+$.ajax({
+url: queryURL,
+method: 'GET'
+}).done(function(response) {
 
+console.log(response);
+var results = response.data;
 
-           // Appending the paragraph and image tag to the personDiv
-           personDiv.append(p);
-           personDiv.append(personImage);
+// Looping through each result item
+for (var i = 0; i < results.length; i++) {
+var gifDiv = $('<div class="item">');
 
-         
-           $("#gifs-appear-here").prepend(personDiv);
-        }
-    });
+var rating = results[i].rating;
 
-  });
+var r = $('<p>').text("Rating: " + rating);
+
+var gifImage = $('<img>');
+gifImage.attr('src', results[i].images.fixed_height_still.url)
+.attr('data-still', results[i].images.fixed_height_still.url)
+.attr('data-animate', results[i].images.fixed_height.url)
+.attr('data-state', "still")
+.addClass("personImage");
+
+	  personDiv.append(p)
+	  personDiv.append(gifImage);
+
+	  $('#gifs-appear-here').prepend(Div);
+	  }
+
+	});
+});
+	
+	
+	$(document).on('click', 'personImage',  function() {
+
+	 var state = $(this).data('state');
+
+	  if (state == "still") {
+	    console.log("It was still");
+	    $(this).attr('src', $(this).data('animate'))
+	    .data('state', 'animate');
+	  } else {
+	    console.log("It was animated");
+	    $(this).attr('src', $(this).data('still'))
+	    .data('state', 'still');               
+	    }
+
+	});
+
+});
